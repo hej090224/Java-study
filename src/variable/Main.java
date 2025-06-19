11065ef7717155ba20ed17e0917d544ac9dab3fe
euijun224 <s25018@gsm.hs.kr>
2025-06-19 17:42:37 +0900

    Java study

---
+++ src/variable/Main.java
@@ -0,0 +1,16 @@
+package variable;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner num = new Scanner(System.in);
+
+        String a = num.next();
+
+
+        System.out.println(a+"??!");
+    }
+}
---
+++ src/variable/Var7.java
@@ -0,0 +1,20 @@
+package variable;
+
+public class Var7 {
+
+    public static void main(String[] args) {
+        int a = 100;        //정수
+        double b = 10.5;    //실수
+        boolean c = false;  //불리언(boolean) true or false
+        char d = 'A';       //문자 하나
+        String e = "Hello Java";    //문자열
+
+        //int z = "백원"
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
