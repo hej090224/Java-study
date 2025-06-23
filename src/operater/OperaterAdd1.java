b9e7dc5cadce03fde4b3562813cbe2808d1f69bc
euijun224 <s25018@gsm.hs.kr>
2025-06-23 23:27:01 +0900

    Java study

---
+++ src/operater/OperaterAdd1.java
@@ -0,0 +1,19 @@
+package operater;
+
+public class OperaterAdd1 {
+    public static void main(String[] args) {
+
+        int a = 0;
+
+        a = a + 1;
+        System.out.println("a = " + a);
+
+        a = a + 1;
+        System.out.println("a = " + a);
+
+        ++a;
+        System.out.println("a = " + a);
+        ++a;
+        System.out.println("a = " + a);
+    }
+}
