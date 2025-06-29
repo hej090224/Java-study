927629b650bf64d53f82c4c773bc54be7eb44bd5
hej090224 <fc49854985@gmail.com>
2025-06-29 15:48:12 +0900

    Java study

--- src/loop/While2_1.java
+++ src/loop/While2_1.java
@@ -7,17 +7,14 @@ public class While2_1 {
         int sum = 0;
         int i = 1;
 
-        sum = sum + i;
-        System.out.println("i = " + i +  " sum = " + sum);
-        i++;
+        sum = sum + 1;
+        System.out.println("i = " + 1 +  " sum = " + sum);
 
-        sum = sum + i;
-        System.out.println("i = " + i +  " sum = " + sum);
-        i++;
+        sum = sum + 2;
+        System.out.println("i = " + 2 +  " sum = " + sum);
 
-        sum = sum + i;
-        System.out.println("i = " + i +  " sum = " + sum);
-        i++;
+        sum = sum + 3;
+        System.out.println("i = " + 3 +  " sum = " + sum);
 
     }
 }
